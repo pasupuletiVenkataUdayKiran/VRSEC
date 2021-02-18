33108547fd711942411339ba49c6f80975ac2569
Uday <pvudaykiran6197@gmail.com>
2021-02-18 10:11:25 +0530

    one.java changed

--- one.java
+++ one.java
@@ -1,5 +1,5 @@
 class hello{
  public static void main(String[] args){
-   System.out.println("welcome to one.java");
+   System.out.println("welcome to one.java and changes are made in this");
  }
 }
